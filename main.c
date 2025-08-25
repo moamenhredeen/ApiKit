96edc49be0e663201e7232b836394bab436f84a4
Moamen Hraden <moamen@hredeen.com>
2025-08-25 18:26:08 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+#include "http_client.h"
+
+int main(int argc, char *argv[]) {
+    if (!glfwInit()) {
+        printf("Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+    if (!window) {
+        printf("Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
